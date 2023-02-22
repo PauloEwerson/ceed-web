3c5403956197dde187450a0ed0c3df38e10b8170
Ewerson Martins <ewersonmrt@gmail.com>
2023-02-22 17:19:22 -0300

    style: Aplica margem aos botões

--- src/src/components/Button/index.jsx
+++ src/src/components/Button/index.jsx
@@ -3,7 +3,13 @@ import React from 'react';
 function Button({ label, primary, onClick }) {
   return (
     <button
-      className={`px-8 py-3 rounded-full font-bold text-white w-9/12
+      className={`
+        px-8 py-3
+        rounded-full
+        font-bold
+        text-white
+        w-9/12
+        m-2
       ${
         primary
         ? 'bg-blue-500'
