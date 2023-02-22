d8cae13754eba247645cc1cf4ba3d7b6b46fa96a
Ewerson Martins <ewersonmrt@gmail.com>
2023-02-22 05:02:30 -0300

    feat: Adiciona estrutura básica do site.
    Cria uma aplicação React e utiliza o Tailwind para estilização.
    Configura as rotas com React Router v6 para as páginas do site.

---
+++ src/src/pages/Depositions/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Depositions() {
+  return (
+    <div>
+      <h1>Depositions</h1>
+    </div>
+  );
+}
+
+export default Depositions;
---
+++ src/src/pages/index.jsx
@@ -0,0 +1,8 @@
+export { default as Home } from './Home';
+export { default as About } from './About';
+export { default as Team } from './Team';
+export { default as Structure } from './Structure';
+export { default as Depositions } from './Depositions';
+export { default as Contact } from './Contact';
+export { default as Header } from './Header';
+export { default as Footer } from './Footer';
