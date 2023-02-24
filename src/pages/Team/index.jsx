b39f24f990b7b239d6ce675fee12fa8a0ec668f6
Ewerson Martins <ewersonmrt@gmail.com>
2023-02-24 04:33:56 -0300

    refactor: Refatoração dos botões de seleção de turma na página Team

--- src/pages/Team/index.jsx
+++ src/pages/Team/index.jsx
@@ -44,7 +44,7 @@ export default function Team() {
   const handleOptionSelect = (option) => {
     setSelectedOption(option);
   };
-  
+
   return (
     <div className="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-16">
       <div className="container mx-auto px-4 sm:px-6 md:px-8 grid grid-cols-1 md:grid-cols-2 gap-8">
@@ -55,41 +55,16 @@ export default function Team() {
           <h3 className="text-3xl text-title-blue font-bold leading-none mb-3">Conheça as <span className="text-yellow-500">mentes brilhantes</span> por trás do sucesso <span className="text-yellow-500">da nossa escola</span>.</h3>
           <p className="text-white mb-8">Selecione a turma abaixo.</p>
           <section className="flex flex-row flex-wrap justify-center items-center text-white">
-            <button className={`text-lg font-bold w-1/3 h-12 bg-blue-500 flex justify-center items-center rounded-lg mx-2 my-2 ${selectedOption === 'Maternalzinho' && 'border-4 border-yellow-500/100'}`} onClick={() => {
-              handleOptionSelect('Maternalzinho')
-            }}>
-              Maternalzinho
-            </button>
-            <button className={`text-lg font-bold w-1/3 h-12 bg-blue-500 flex justify-center items-center rounded-lg mx-2 my-2 ${selectedOption === 'Maternal' && 'border-4 border-yellow-500/100'}`} onClick={() => {
-              handleOptionSelect('Maternal')
-            }}>
-              Maternal
-            </button>
-            <button className={`text-lg font-bold w-1/3 h-12 bg-blue-500 flex justify-center items-center rounded-lg mx-2 my-2 ${selectedOption === 'Jardim I' && 'border-4 border-yellow-500/100'}`} onClick={() => {
-              handleOptionSelect('Jardim I')
-            }}>
-              Jardim I
-            </button>
-            <button className={`text-lg font-bold w-1/3 h-12 bg-blue-500 flex justify-center items-center rounded-lg mx-2 my-2 ${selectedOption === 'Jardim II' && 'border-4 border-yellow-500/100'}`} onClick={() => {
-              handleOptionSelect('Jardim II')
-            }}>
-              Jardim II
-            </button>
-            <button className={`text-lg font-bold w-1/3 h-12 bg-blue-500 flex justify-center items-center rounded-lg mx-2 my-2 ${selectedOption === '1 Ano' && 'border-4 border-yellow-500/100'}`} onClick={() => {
-              handleOptionSelect('1 Ano')
-            }}>
-              1 Ano
-            </button>
-            <button className={`text-lg font-bold w-1/3 h-12 bg-blue-500 flex justify-center items-center rounded-lg mx-2 my-2 ${selectedOption === '2 Ano' && 'border-4 border-yellow-500/100'}`} onClick={() => {
-              handleOptionSelect('2 Ano')
-            }}>
-              2 Ano
-            </button>
-            <button className={`text-lg font-bold w-1/3 h-12 bg-blue-500 flex justify-center items-center rounded-lg mx-2 my-2 ${selectedOption === '3 Ano' && 'border-4 border-yellow-500/100'}`} onClick={() => {
-              handleOptionSelect('3 Ano')
-            }}>
-              3 Ano
-            </button> 
+            {Object.keys(optionsToImages).map((option, index) => (
+              <button
+                key={index}
+                className={`text-lg font-bold w-1/3 h-12 bg-blue-500 flex justify-center items-center rounded-lg mx-2 my-2 ${selectedOption === option && 'border-4 border-yellow-500/100'
+                  }`}
+                onClick={() => handleOptionSelect(option)}
+              >
+                {option}
+              </button>
+            ))}
           </section>
         </div>
         <div className="flex justify-center">
